4c06030dc9435806a17f59132e808df9ebd92caf
HigorBernardesPNG <zdtshark@gmail.com>
2026-02-24 16:08:22 -0400

    Update Estudo.js

--- EstudoJSGenerico/Estudo.js
+++ EstudoJSGenerico/Estudo.js
@@ -817,3 +817,19 @@ console.log(retornaObjeto(
     {valor:53,pago:true},
   ]))
 // ------------------------------------------------------
+
+
+
+// --------------------------------------------------------BanckENDEstudos--------------------------------------------------------------------------------------------
+
+
+function validaPedido(pedido){
+  
+  if(array.isArray(pedido)) return {ok:false, error:"INVALID_INPUT"};
+  if(pedido.length === 0) return {ok:false, error:"EMPTY_LIST"};
+  if(typeof pedido.valor != "number" && typeof pedido.pago != "boolean") return {ok:false, error:"INVALID_ORDER"};
+  return {ok:true, value:pedido.pago}
+  
+}
+
+console.log(validaPedido({valor:32,pago:true}));
